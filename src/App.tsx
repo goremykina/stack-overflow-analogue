f165f8bdb5d640a975020242caa41485386f61cc
Alina Goremykina <a.goremykina98@gmail.com>
2025-04-03 17:00:23 +0200

    Changed App

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,27 @@ import RegisterPage from "./pages/register/RegisterPage.tsx";
 import HomePage from "./pages/home/HomePage.tsx";
 import Header from "./components/header/Header.tsx";
 import SideBar from "./components/side-bar/SideBar.tsx";
-import Sidebar from "./components/side-bar/SideBar.tsx";
+import { Box, CssBaseline } from "@mui/material";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePage />
-    },
-    {
-        path: "/register",
-        element: <RegisterPage />
-    }
+    { path: "/", element: <HomePage /> },
+    { path: "/register", element: <RegisterPage /> }
 ])
 
 function App() {
-    console.log(SideBar)
-
-  return (
-      <div>
-          <Header/>
-          <Sidebar />
-          <div>
-              <RouterProvider router={router} />
-          </div>
-      </div>
-  )
+    return (
+        <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+            <Header />
+            <CssBaseline />
+            <Box sx={{ display: "flex", flexGrow: 1, minHeight: "0" }}>
+                <SideBar />
+                <Box sx={{ flexGrow: 1, padding: 3, overflow: "auto", minHeight: 0 }}>
+                    <RouterProvider router={router} />
+                </Box>
+            </Box>
+        </Box>
+    );
 }
+
 
 export default App
