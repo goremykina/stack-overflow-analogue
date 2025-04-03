7170b9ffb636b8c8febdf2697217a6d6e492efc5
Alina Goremykina <a.goremykina98@gmail.com>
2025-04-03 18:04:47 +0200

    Fix header and add select

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,42 @@
-// src/components/Header.tsx
-// import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Select } from '@mui/material';
+import { useState } from 'react';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    Select,
+    FormControl,
+    SelectChangeEvent,
+    MenuItem
+} from '@mui/material';
 
 const Header = () => {
+    const [language, setLanguage] = useState('EN');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setLanguage(event.target.value);
+    };
+
     return (
-        <AppBar position="sticky">
+        <AppBar position="sticky" sx={{ padding: 0.5}}>
             <Toolbar>
                 <Typography variant="h6" sx={{ flexGrow: 1 }}>
                     Codelang
                 </Typography>
+
                 <Button color="inherit">Login</Button>
-                <Select label={'lang'}></Select>
+
+                <FormControl sx={{ width: 100, marginLeft: 3}}>
+                    <Select
+                        value={language}
+                        onChange={handleChange}
+                    >
+                        <MenuItem value='RU'>RU</MenuItem>
+                        <MenuItem value='PL'>PL</MenuItem>
+                        <MenuItem value='EN'>EN</MenuItem>
+                    </Select>
+                </FormControl>
+
             </Toolbar>
         </AppBar>
     );
