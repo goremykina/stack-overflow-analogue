40017b413725ad18af84fd53fec3fc9401673205
Alina Goremykina <a.goremykina98@gmail.com>
2025-04-05 22:52:55 +0200

    Post draft

---
+++ src/components/post/Post.tsx
@@ -0,0 +1,85 @@
+import { FC, useState } from 'react';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import { CardContent, Card, Box, IconButton, Typography } from "@mui/material";
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
+import CommentIcon from '@mui/icons-material/Comment';
+import CodeIcon from '@mui/icons-material/Code';
+
+interface PostProps {
+    code: string,
+    language: string,
+}
+
+const Post: FC<PostProps> = ({ code, language }) => {
+    const [likesCount, setLikesCount] = useState(0);
+    const [dislikesCount, setDislikesCount] = useState(0)
+    const [commentCount, setCommentCount] = useState(0)
+
+    return (
+        <Card>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', padding: '1rem' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: '.5rem' }}>
+                    <PersonOutlineIcon/>
+                    User
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: '.5rem' }}>
+                    <CodeIcon />
+                    {language}
+                </Box>
+            </Box>
+
+            <CardContent>
+                <Box
+                    component="pre"
+                    sx={{
+                        backgroundColor: '#f5f5f5',
+                        padding: 2,
+                        borderRadius: 1,
+                        overflowX: 'scroll'
+                    }}
+                >
+                    {code}
+                </Box>
+
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Box sx={{
+                        display: 'flex'
+                    }}>
+                        <IconButton
+                            sx={{ display: 'flex', gap: 1 }}
+                            onClick={() => setLikesCount(likesCount + 1)}
+                        >
+                            <Typography>{likesCount}</Typography>
+                            <ThumbUpOffAltIcon />
+                        </IconButton>
+                        <IconButton
+                            sx={{ display: 'flex', gap: 1 }}
+                            onClick={() => setDislikesCount(dislikesCount - 1)}
+                        >
+                            <Typography>{dislikesCount}</Typography>
+                            <ThumbDownOffAltIcon />
+                        </IconButton>
+                    </Box>
+                    <Box>
+                        <IconButton
+                            sx={{ display: 'flex', gap: 1 }}
+                            onClick={() => setCommentCount(commentCount +1)}
+                        >
+                            <Typography>{commentCount}</Typography>
+                            <CommentIcon />
+                        </IconButton>
+                    </Box>
+                </Box>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default Post;
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,20 @@
-// import React from 'react';
+import { useEffect, useState } from "react";
+import { fetchPosts } from "../../api/posts.ts";
+import Post from "../../components/post/Post.tsx";
+import { PostModel } from "../../models/post.model.ts";
 
 const HomePage = () => {
+    const [posts, setPosts] = useState<PostModel[]>([]);
+
+    useEffect(() => {
+        fetchPosts()
+            .then(response => setPosts(response.data));
+    }, []);
+
     return (
-        <div>
-            Welcome to Codelang!
-        </div>
+        <>
+            {posts.map(post => (<Post language={post.language} code={post.code} />))}
+        </>
     );
 };
 
