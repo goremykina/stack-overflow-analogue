4e47aa962ed370853cc775aac264fe540908a318
Alina Goremykina <a.goremykina98@gmail.com>
2025-04-10 12:15:07 +0200

    Add parameters to fetch

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -2,8 +2,8 @@ import { api } from './api';
 import { Post } from "../models/post.model.ts";
 import { QueryResponse } from "../models/response.model.ts";
 
-export const fetchPosts = async () => {
-    const response = await api.get<QueryResponse<Post[]>>('snippets');
+export const fetchPosts = async (page: number = 1) => {
+    const response = await api.get<QueryResponse<Post[]>>('snippets', { searchParams: { page } });
     const { data } = await response.json();
 
     return data;
