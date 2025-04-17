04c5957c70328e5e8c1ac8cb37e315d1ef379aff
Alina Goremykina <a.goremykina98@gmail.com>
2025-04-17 15:18:48 +0200

    Add spinner to  the edit post page

--- src/pages/edit-post/EditPostPage.tsx
+++ src/pages/edit-post/EditPostPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import {
     Box,
-    Button,
+    Button, CircularProgress,
     MenuItem,
     Select,
     Typography
@@ -87,73 +87,86 @@ const EditPostPage = () => {
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: '3rem' }}>
-            <Typography sx={{ textAlign: 'center', fontSize: '2rem' }}>
-                {!editMode
-                    ? 'Create new snippet!'
-                    : 'Change snippet'
-                }
-
-            </Typography>
-
-            <form onSubmit={handleSubmit(handleQuestion)} style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-                <Typography sx={{ fontSize: '1.1rem', fontWeight: '600' }} component={'h3'}>Language of your snippet:</Typography>
-                <Controller
-                    control={control}
-                    name="language"
-                    render={({
-                        field: { onChange, onBlur, value, name },
-                    }) => (
-
-                        <Select
-                            name={name}
-                            value={value}
-                            onChange={onChange}
-                            onBlur={onBlur}
-                        >
-                            {availableLanguages.map((lang, index) => (
-                                <MenuItem value={lang} key={index}>{lang}</MenuItem>
-                            ))}
-                        </Select>
-                    )}
-                />
-
-                <Typography sx={{ fontSize: '1.1rem', fontWeight: '600' }}>Code of your snippet:</Typography>
-
-                <Controller
-                    control={control}
-                    name="attachedCode"
-                    render={({
-                        field: { onChange, onBlur, value, name },
-                    }) => (
-                        <Editor
-                            name={name}
-                            value={value}
-                            onValueChange={onChange}
-                            onBlur={onBlur}
-                            highlight={(code) => highlight(code, languages.javascript, 'javascript')}
-                            padding={10}
-                            style={{
-                                minHeight: '200px',
-                                backgroundColor: "#f5f5f5",
-                                overflow: 'auto'
-                            }}
-                        />
-                    )}
-                />
-
-                <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    fullWidth sx={{ mt: 2 }}
-                    loading={loading}
+            {loading
+                ? <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '60vh'
+                    }}
                 >
-                    {!editMode
-                        ? 'CREATE SNIPPET'
-                        : 'EDIT SNIPPET'
-                    }
-                </Button>
-            </form>
+                    <CircularProgress size={60} />
+                </Box>
+                : <>
+                    <Typography sx={{ textAlign: 'center', fontSize: '2rem' }}>
+                        {!editMode
+                            ? 'Create new snippet!'
+                            : 'Change snippet'
+                        }
+
+                    </Typography>
+                    <form onSubmit={handleSubmit(handleQuestion)} style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+                        <Typography sx={{ fontSize: '1.1rem', fontWeight: '600' }} component={'h3'}>Language of your snippet:</Typography>
+                        <Controller
+                            control={control}
+                            name="language"
+                            render={({
+                                         field: { onChange, onBlur, value, name },
+                                     }) => (
+
+                                <Select
+                                    name={name}
+                                    value={value}
+                                    onChange={onChange}
+                                    onBlur={onBlur}
+                                >
+                                    {availableLanguages.map((lang, index) => (
+                                        <MenuItem value={lang} key={index}>{lang}</MenuItem>
+                                    ))}
+                                </Select>
+                            )}
+                        />
+
+                        <Typography sx={{ fontSize: '1.1rem', fontWeight: '600' }}>Code of your snippet:</Typography>
+
+                        <Controller
+                            control={control}
+                            name="attachedCode"
+                            render={({
+                                         field: { onChange, onBlur, value, name },
+                                     }) => (
+                                <Editor
+                                    name={name}
+                                    value={value}
+                                    onValueChange={onChange}
+                                    onBlur={onBlur}
+                                    highlight={(code) => highlight(code, languages.javascript, 'javascript')}
+                                    padding={10}
+                                    style={{
+                                        minHeight: '200px',
+                                        backgroundColor: "#f5f5f5",
+                                        overflow: 'auto'
+                                    }}
+                                />
+                            )}
+                        />
+
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            fullWidth sx={{ mt: 2 }}
+                            loading={loading}
+                        >
+                            {!editMode
+                                ? 'CREATE SNIPPET'
+                                : 'EDIT SNIPPET'
+                            }
+                        </Button>
+                    </form>
+                </>
+            }
         </Box>
     );
 };
